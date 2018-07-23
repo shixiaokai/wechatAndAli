52f619d3f59f75d68a595695643c70068a953147
shixiaokai <shixiaokai@e7124.com>
2018-07-23 11:56:57 +0800

    test

--- app/lib/WeChat/H5pay/WxPay.Data.php
+++ app/lib/WeChat/H5pay/WxPay.Data.php
@@ -148,14 +148,14 @@ class WxPayResults extends WxPayDataBase
 	{
 		//fix异常
 		if(!$this->IsSignSet()){
-			throw new WxPayException("签名错误！");
+			throw new \WxPayException("签名错误！");
 		}
 		
 		$sign = $this->MakeSign();
 		if($this->GetSign() == $sign){
 			return true;
 		}
-		throw new WxPayException("签名错误！");
+		throw new \WxPayException("签名错误！");
 	}
 	
 	/**
